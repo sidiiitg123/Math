bb72421b748ef08bd43f8582eb9dfad41d3952dd
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2020-04-12 04:30:19 +0530

    use String Builder

---
+++ BinaryRep/src/com/company/BinaryRep.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class BinaryRep {
+    public String findDigitsInBinary(int A) {
+        StringBuilder s =new StringBuilder();
+        if(A==0){
+            return "0";
+        }
+        while(A>0){
+            int remain=A%2;
+            s.append(remain);
+            A=A/2;
+        }
+        return  s.reverse().toString();
+    }
+}
