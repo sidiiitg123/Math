562c514d6bb922605db29ed51eac3dee4fd102d6
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2020-04-15 06:27:54 +0530

    completed

--- sorted rank/src/com/company/SortedRank.java
+++ sorted rank/src/com/company/SortedRank.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class SortedRank {
+    public int fact(int x){
+        if(x<=1){
+            return 1;
+        }
+        return x*fact(x-1);
+    }
+
+    public int findRight(String A,int low,int high){
+        int count=0;
+        for(int j=low+1;j<=high;j++){
+            if(A.charAt(j)<A.charAt(low)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int findRank(String A){
+        int rank=1;
+        int noRight=0;
+        for(int i=0;i<A.length();i++){
+            noRight=findRight(A,i,A.length()-1);
+            rank+=noRight*fact(A.length()-i-1);
+        }
+        return  rank;
+    }
 }
